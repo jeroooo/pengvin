d30ff010929062ed3a3f819fdebe80d79e0dd710
jeroooo <64100349+jeroooo@users.noreply.github.com>
2024-03-12 11:44:36 +0100

    Update app.js

--- app.js
+++ app.js
@@ -24,10 +24,11 @@ loadImages();
 
 
 // listen for scroll event and load more images if we reach the bottom of window
-window.addEventListener('scroll',()=>{
+/*window.addEventListener('scroll',()=>{
     console.log("scrolled", window.scrollY) //scrolled from top
     console.log(window.innerHeight) //visible part of screen
     if(window.scrollY + window.innerHeight >= document.documentElement.scrollHeight){
         loadImages();
     }
 })
+*/
